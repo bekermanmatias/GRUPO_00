65416655c1ad884a2843e5de6ebd5023f91cc353
Matias Rau Bekerman <bekermanmatias@gmail.com>
2025-04-13 17:43:43 -0300

    initial commit tp1azul

---
+++ TP1Azul/app/src/main/java/com/example/app1/ui/theme/Color.kt
@@ -0,0 +1,17 @@
+package com.example.app1.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryBlue = Color(0xFF0D47A1)     // Azul principal
+val SecondaryGray = Color(0xFFB0BEC5)   // Gris secundario
+val BackgroundWhite = Color(0xFFFFFFFF) // Fondo blanco
+val TextBlack = Color(0xFF000000)       // Texto negro opcional
+
+/////
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
